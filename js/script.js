f4221ebe3e008695287686dcb8e986d61f51ddbb
Augusto Henrique <augustokun13@gmail.com>
2026-03-04 14:29:12 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {
+    scrollFunction();
+    updateProgressBar();
+};
+
+function updateProgressBar() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
